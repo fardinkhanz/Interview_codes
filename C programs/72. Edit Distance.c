f903e36d156ec06ce380e6292c99e764abc3c289
Fardin Khan <89691395+fardinkhanz@users.noreply.github.com>
2023-09-23 09:27:02 +0530

    Add files via upload

---
+++ C programs/72. Edit Distance.c
@@ -0,0 +1,48 @@
+int min(int a,int b,int c)
+{
+    int min=a;
+    if(b<min)
+    {
+        min=b;
+
+    }
+    if(min>c)
+    {
+        min=c;
+
+    }
+    return min;
+}
+
+
+
+int minDistance(char* word1, char* word2) {
+int m = strlen(word1);
+int n = strlen(word2);
+int dp[m+1][n+1];
+
+for(int i=0;i<=m;i++)
+{
+    for(int j=0;j<=n;j++)
+    {
+        if(i==0)
+        {
+            dp[i][j]=j;
+        }
+        else if(j==0)
+        {
+            dp[i][j]=i;
+        }
+        else if(word1[i - 1] == word2[j - 1])
+        {
+            dp[i][j]=dp[i-1][j-1];
+        }
+        else
+        {
+            dp[i][j]=1+min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1]);
+        }
+
+    }
+}
+return dp[m][n];
+}
