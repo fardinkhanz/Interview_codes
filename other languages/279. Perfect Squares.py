6b5ca37d082b1511b2b9930c8a124807ab43c6a7
Fardin Khan <89691395+fardinkhanz@users.noreply.github.com>
2024-02-11 13:55:44 +0530

    Create 279. Perfect Squares.py

---
+++ other languages/279. Perfect Squares.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp=[n]*(n+1)
+        dp[0]=0
+        for target in range(1,n+1):
+            for s in range(1,target + 1):
+                sq=s*s
+                if target-sq < 0:
+                    break
+                dp[target]=min(dp[target],dp[target-sq]+1)
+        return dp[n]        
+
+        
